9ed3450aa366050f5153158a0765b62184b16365
Denis <eltimccc@yandex.ru>
2022-05-12 10:54:29 +0300

    запушиваю

---
+++ yatube/posts/models.py
@@ -0,0 +1,25 @@
+#models.py
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    group = models.ForeignKey('Group', blank=True, null=True, on_delete=models.CASCADE)
+
+
+class Group(models.Model):
+    title: str = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=150, unique=True)
+    description = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.title
