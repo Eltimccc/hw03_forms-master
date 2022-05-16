56534f61ed986efa753046283790811409e82645
Denis <eltimccc@yandex.ru>
2022-05-16 11:49:10 +0300

    Третья правки

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+
 from .models import Post, Group
 
 
@@ -12,7 +13,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group)[:10]
+    posts = Post.objects.all()[:10]
     context = {
         'group': group,
         'posts': posts,
