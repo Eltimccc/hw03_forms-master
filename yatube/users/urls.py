9bdb20cae625ac4aceedc31af030b78b7cbc4308
Denis <eltimccc@yandex.ru>
2022-05-26 18:38:21 +0300

    вставил в urls ResetView итд

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,6 +1,13 @@
 # users/urls.py
 # Импортируем из приложения django.contrib.auth нужный view-класс
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+                                LoginView, 
+                                LogoutView, 
+                                PasswordResetView, 
+                                PasswordResetDoneView, 
+                                PasswordResetConfirmView, 
+                                PasswordResetCompleteView,
+                                )
 from django.urls import path
 
 from . import views
@@ -8,7 +15,9 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    path(
+        'signup/', views.SignUp.as_view(), 
+        name='signup'),
     path(
         'logout/',
         LogoutView.as_view(template_name='users/logged_out.html'),
@@ -19,4 +28,20 @@ urlpatterns = [
         LoginView.as_view(template_name='users/login.html'),
         name='login'
     ),
-]  
+    path(
+        'password-reset/', 
+        PasswordResetView.as_view(
+        template_name='users/password_reset_form.html')),
+    path(
+        'password-reset/done/', 
+        PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html')),
+    path(
+        'reset/<uidb64>/<token>/', 
+        PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html')),
+    path(
+        'reset/done/',
+        PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html')),
+        ]  
